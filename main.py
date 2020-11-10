33f6fcecb4c5ebcd2b4611337dd97eea48ee1d08
Chris-gqz <1395523356@qq.com>
2020-11-10 14:23:05 +0800

    代码格式化

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # 从日志文件下获取日志名称
 def getFileNamesByLogFile(logFilePath):
     # 将路径下的文件名倒入到字典中
-    fileNamesDict={}
+    fileNamesDict = {}
 
     for _, _, fileNames in os.walk(logFilePath):
         for fileName in fileNames:
-            fileNamesDict[fileName.split(".")[0]]="1"
+            fileNamesDict[fileName.split(".")[0]] = "1"
 
     return fileNamesDict
+
 
 # 从配置文件获取索引名称
 def getIndexNamesByConfig(configFilePath):
@@ -20,21 +21,22 @@ def getIndexNamesByConfig(configFilePath):
         if "type =>" in line:
             indexNamesDict[line.split('"')[1]] = "1"
 
-    return  indexNamesDict
+    return indexNamesDict
+
 
 # 更新配置文件
 def updateConfig(configFilePath, logFilePath, fileNamesDict):
-    fileInfo = open(configFilePath,"w")
+    fileInfo = open(configFilePath, "w")
     fileInfo.truncate()
 
     input = ""
     output = ""
 
     for fileName in fileNamesDict:
-        inputStr = "input { \n file { \n path => [\""+logFilePath+"/"+fileName+".*"+"\"] \n type => \""+fileName+"\" \n}\n}\n"
+        inputStr = "input { \n file { \n path => [\"" + logFilePath + "/" + fileName + ".*" + "\"] \n type => \"" + fileName + "\" \n}\n}\n"
         input += inputStr
 
-        outputStr = "if [type] == \""+fileName+"\" { \n elasticsearch{ \n hosts => [\"http://localhost:9200\"] \n index => \""+fileName.lower()+"\"\n}\n}\n"
+        outputStr = "if [type] == \"" + fileName + "\" { \n elasticsearch{ \n hosts => [\"http://localhost:9200\"] \n index => \"" + fileName.lower() + "\"\n}\n}\n"
         output += outputStr
 
     fileInfo.write(input)
@@ -46,9 +48,8 @@ def updateConfig(configFilePath, logFilePath, fileNamesDict):
     return
 
 
-
 # 判断索引名称是否都包含文件名称，不包含则更新配置文件
-def upDateConfigIfNeed(logFilePath,fileNamesDict,configFilePath,indexNamesDict):
+def upDateConfigIfNeed(logFilePath, fileNamesDict, configFilePath, indexNamesDict):
     for fileName in fileNamesDict:
         if not indexNamesDict.has_key(fileName):
             print("需要更新！")
@@ -56,22 +57,15 @@ def upDateConfigIfNeed(logFilePath,fileNamesDict,configFilePath,indexNamesDict):
             break
         print("无需更新")
 
+
 import os
 
 # 日志文件位置
-LogFilePath='/Users/rylink/ELK/log'
-FileNamesDict=getFileNamesByLogFile(LogFilePath)
+LogFilePath = '/Users/rylink/ELK/log'
+FileNamesDict = getFileNamesByLogFile(LogFilePath)
 
 # 配置文件位置
-ConfigFilePath='/Users/rylink/ELK/logstash-7.9.3/logstashUpdate.conf'
-IndexNamesDict=getIndexNamesByConfig(ConfigFilePath)
+ConfigFilePath = '/Users/rylink/ELK/logstash-7.9.3/logstashUpdate.conf'
+IndexNamesDict = getIndexNamesByConfig(ConfigFilePath)
 
-upDateConfigIfNeed(LogFilePath,FileNamesDict,ConfigFilePath,IndexNamesDict)
-
-
-
-
-
-
-
-
+upDateConfigIfNeed(LogFilePath, FileNamesDict, ConfigFilePath, IndexNamesDict)
